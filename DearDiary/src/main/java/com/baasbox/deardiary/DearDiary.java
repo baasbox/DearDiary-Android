b74f29c197946e15793cfe90655196e25dee315b
aKtor <eliantor@gmail.com>
2014-01-24 10:32:25 +0100

    initial project commit

---
+++ DearDiary/src/main/java/com/baasbox/deardiary/DearDiary.java
@@ -0,0 +1,14 @@
+package com.baasbox.deardiary;
+
+import android.app.Application;
+
+/**
+ * Created by Andrea Tortorella on 24/01/14.
+ */
+public class DearDiary extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
