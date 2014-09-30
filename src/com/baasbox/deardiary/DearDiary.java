4732118585aae37d6545ebfdfd9593315dff0da7
Andrea Tortorella <eliantor@gmail.com>
2014-09-30 10:47:52 +0200

    fixed compatibility

--- src/com/baasbox/deardiary/DearDiary.java
+++ src/com/baasbox/deardiary/DearDiary.java
@@ -15,11 +15,16 @@ public class DearDiary extends Application {
     public void onCreate() {
         super.onCreate();
         //todo 1.2
-        BaasBox.Config config = new BaasBox.Config();
-        config.authenticationType= BaasBox.Config.AuthType.SESSION_TOKEN;
-        config.apiDomain = "192.168.56.1"; // the host address
-        config.httpPort=9000;
-        BaasBox.initDefault(this,config);
+        BaasBox.builder(this).setAuthentication(BaasBox.Config.AuthType.SESSION_TOKEN)
+                .setApiDomain("10.0.2.0")
+                .setPort(9000)
+                .setAppCode("1234567890")
+                .init();
+//        BaasBox.Config config = new BaasBox.Config();
+//        config.authenticationType= BaasBox.Config.AuthType.SESSION_TOKEN;
+//        config.apiDomain = "192.168.56.1"; // the host address
+//        config.httpPort=9000;
+//        BaasBox.initDefault(this,config);
     }
 
 }
